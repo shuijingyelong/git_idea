a9358e22c65087e611521dbb49d5a7a01e403344
qhl <2867478629@qq.com>
2019-09-21 12:26:40 +0800

    qhl修改bug

--- src/main/java/cn/edu/cdu/git/Dugu.java
+++ src/main/java/cn/edu/cdu/git/Dugu.java
@@ -4,6 +4,10 @@ public class Dugu {
 
     public static void main(String[] args) {
         System.out.println("jinan");
+        System.out.println("jinan");
+        System.out.println("jinan");
+
+
     }
 
 
